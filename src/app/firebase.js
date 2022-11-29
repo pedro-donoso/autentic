b78b3e16fc1bd4cc3fbe3310cc41bd6f5ca5deb5
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-11-29 16:05:59 -0300

    auth

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,7 @@
  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,5 @@
 
   // Initialize Firebase
  export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
   
--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,12 +1,25 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+
+import { auth } from './firebase.js';
+
 const signupForm = document.querySelector('#signup-form');
 
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener('submit', async (e) => {
     e.preventDefault()
 
 const email = signupForm['signup-email'].value
 const password = signupForm['signup-password'].value
     
-console.log(email, password);
+    console.log(email, password);
+    
+    try {
+        
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredentials);
+
+    } catch (error) {
+        console.log(error);
+    }
 
 })
 
