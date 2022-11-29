0d8786cde40fe97c37c41f564c6db930dfcc0479
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-11-29 16:31:09 -0300

    valido

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,6 +1,6 @@
  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
 
 import { auth } from './firebase.js';
 
