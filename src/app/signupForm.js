fa6e064aa502ec832017dfd920f6ce115714517e
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-11-29 15:14:42 -0300

    value

--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -3,8 +3,10 @@ const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-const email = signupForm['signup-email']
-    signupForm['signup-password']
+const email = signupForm['signup-email'].value
+const password = signupForm['signup-password'].value
+    
+console.log(email, password);
 
 })
 
