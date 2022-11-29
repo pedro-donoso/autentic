a1172aef27780486412e151c56a7a70fc305001e
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2022-11-29 16:57:37 -0300

    registro

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,5 +1,5 @@
  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
--- src/app/signupForm.js
+++ src/app/signupForm.js
@@ -4,13 +4,13 @@ import { auth } from './firebase.js';
 
 const signupForm = document.querySelector('#signup-form');
 
-signupForm.addEventListener('submit', async (e) => {
+signupForm.addEventListener('submit', async(e) => {
     e.preventDefault()
 
 const email = signupForm['signup-email'].value
 const password = signupForm['signup-password'].value
     
-    console.log(email, password);
+    console.log(email, password)
     
     try {
         
